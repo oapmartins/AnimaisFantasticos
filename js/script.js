239e23ecd1d016f817c17dc7944751327ff4a0b2
Octávio Martins <oapmartins@sga.pucminas.br>
2021-06-20 11:56:05 -0300

    Anima Números
    Adicionando função para animar os números da section números.

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@ import initModal from './modules/modal.js';
 import initTooltip from './modules/tooltip.js';
 import initDtropdownMenu from './modules/dropdown-menu.js';
 import initMenuMobile from './modules/menu-mobile.js';
+import initAnimaNumeros from './modules/anima-numeros.js';
 
 initTabNav();
 initAccordion();
@@ -15,6 +16,7 @@ initModal();
 initTooltip();
 initDtropdownMenu();
 initMenuMobile();
+initAnimaNumeros();
 
 
 
