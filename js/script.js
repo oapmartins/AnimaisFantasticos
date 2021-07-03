1b9b0992d1306f64fbfd8baf376759e7d9b4b313
Octávio Martins <oapmartins@sga.pucminas.br>
2021-07-03 16:41:34 -0300

    Atualizando o anima números
    Adicionando o nome e os números dos animais para puxarem de uma API Fake.

--- js/script.js
+++ js/script.js
@@ -6,8 +6,8 @@ import initModal from './modules/modal.js';
 import initTooltip from './modules/tooltip.js';
 import initDtropdownMenu from './modules/dropdown-menu.js';
 import initMenuMobile from './modules/menu-mobile.js';
-import initAnimaNumeros from './modules/anima-numeros.js';
 import initFuncionamento from './modules/funcionamento.js';
+import initFetchAnimais from './modules/fetch-animais.js';
 
 initTabNav();
 initAccordion();
@@ -17,11 +17,8 @@ initModal();
 initTooltip();
 initDtropdownMenu();
 initMenuMobile();
-initAnimaNumeros();
 initFuncionamento();
+initFetchAnimais();
 
 
 
-
-
- 
