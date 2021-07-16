8919d9da6a2ecbc96db476bad9ead4f26fa421f6
Octávio Martins <octaviomartins10@gmail.com>
2021-07-15 23:06:22 -0300

    Refatoração do dropdown utilizando classes no js.

--- js/modules/dropdown-menu.js
+++ js/modules/dropdown-menu.js
@@ -1,20 +1,43 @@
 import outsideClick from './outside-click.js';
 
-export default function initDtropdownMenu() {
-    const dropdownMenus = document.querySelectorAll('[data-dropdown]');
+export default class DropdownMenu {
 
-    function hadleClick(event) {
+    constructor(dropdownMenus, events) {
+        this.dropdownMenus = document.querySelectorAll(dropdownMenus);
+
+        // Define touchstart e click como argumento padrão de events caso o usuário não defina.
+        if(events === undefined) this.events = ['touchstart', 'click'];
+        else this.events = events;
+
+        this.activeClass = 'active';
+        this.activeDropdownMenu = this.activeDropdownMenu.bind(this);
+    }
+
+    // Ativa o dropdown menu e ativa a opção que observa o click fora dele.
+    activeDropdownMenu(event) {
         event.preventDefault();
-        this.classList.add('active');
+        const element = event.currentTarget;
 
-        outsideClick(this, ['touchstart', 'click'], () => {
-            this.classList.remove('active');
-        });
-    } 
+        element.classList.add(this.activeClass);
 
-    dropdownMenus.forEach(item => {
-        ['touchstart', 'click'].forEach(userEvent => {
-            item.addEventListener(userEvent, hadleClick)
+        outsideClick(element, this.events, () => {
+            element.classList.remove(this.activeClass);
         });
-    });
+    }
+
+    // Adiciona os eventos ao dropdown menu.
+    addDropdownMenusEvent() {
+        this.dropdownMenus.forEach(item => {
+            this.events.forEach(userEvent => {
+                item.addEventListener(userEvent, this.activeDropdownMenu)
+            });
+        });
+    }
+
+    init() {
+        if (this.dropdownMenus.length) {
+            this.addDropdownMenusEvent();
+            return this;
+        }
+    }
 }
